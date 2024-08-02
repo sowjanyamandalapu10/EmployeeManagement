a6a4035c03b67ce33ba462dd7a445f34185eace9
Sowjanya Mandalapu <sowjanyamandalapu10@gmail.com>
2024-08-02 12:06:31 +0530

    Client Model is added

---
+++ EmployeeManagement/EmployeeManagement/Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class EmployeeModel
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Location {  get; set; }
+        public string Email {  get; set; }
+        public string Department {  get; set; }
+        public string Qualification {  get; set; }
+        public string CreateData { get; set; }
+        public string UpdateData { get; set; }
+    }
+}
